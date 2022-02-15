7e1d349f9040b3089a03caec4324a9bfb2965fa9
Zeffo <zeffo@nay.am>
2022-02-16 01:56:59 +0530

    Fixed embed script

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -29,12 +29,12 @@ const Layout = ({ children }) => {
         {children}
       </main>
       <script
-        defer
         data-name="BMC-Widget"
         data-cfasync="false"
         src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
         data-id="maglit"
         data-description="Support me on Buy me a coffee!"
+        data-message=""
         data-color="#FFDD00"
         data-position="Right"
         data-x_margin="18"
