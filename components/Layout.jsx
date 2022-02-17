f65370f6de27587648cd01e1b272d614e67c9a1b
Zeffo <zeffo@nay.am>
2022-02-18 03:13:17 +0530

    Added loading

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,47 +1,63 @@
-import { useRecoilState } from "recoil";
+import GridLoader from "react-spinners/GridLoader";
 import { navbarState } from "../atoms/navbarAtom";
 import { BsFillGridFill } from "react-icons/bs";
-import Script from "next/script";
+import { useRecoilState } from "recoil";
+import React, { useEffect, useState } from "react";
 import Navbar from "./Navbar";
-import React from "react";
 
 const Layout = ({ children }) => {
   const [navbarOpen, setNavbarOpen] = useRecoilState(navbarState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
   return (
-    <div
-      className={`${
-        navbarOpen ? "overflow-x-hidden" : "overflow-hidden"
-      } animate relative bg-slate-50`}
-    >
-      <Navbar />
-      <main>
-        <button
+    <>
+      {loading ? (
+        <div className="flex h-screen w-screen items-center justify-center">
+          <GridLoader loading={loading} size={15} color={"#ff8100"} />
+        </div>
+      ) : (
+        <div
           className={`${
-            navbarOpen ? "scale-0" : "scale-100"
-          } animate absolute top-5 right-5 z-10 rounded-sm bg-slate-50 text-xl text-slate-400 hover:text-blue-500 focus:text-blue-500 md:text-3xl`}
-          onClick={() => {
-            setNavbarOpen(!navbarOpen);
-          }}
+            navbarOpen ? "bg-yell overflow-x-hidden" : "overflow-hidden"
+          } animate relative bg-slate-50`}
         >
-          <BsFillGridFill />
-        </button>
-        {children}
-      </main>
-      <script
-        defer
-        data-name="BMC-Widget"
-        data-cfasync="false"
-        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
-        data-id="maglit"
-        data-description="Support me on Buy me a coffee!"
-        data-message="Hi, Welcome to MagLit!"
-        data-color="#FFDD00"
-        data-position="Right"
-        data-x_margin="18"
-        data-y_margin="18"
-      ></script>
-    </div>
+          <Navbar />
+          <main>
+            <button
+              className={`${
+                navbarOpen ? "scale-0" : "scale-100"
+              } animate absolute top-5 right-5 z-10 rounded-sm bg-slate-50 text-xl text-slate-400 hover:text-blue-500 focus:text-blue-500 md:text-3xl`}
+              onClick={() => {
+                setNavbarOpen(!navbarOpen);
+              }}
+            >
+              <BsFillGridFill />
+            </button>
+            {children}
+          </main>
+          <script
+            defer
+            data-name="BMC-Widget"
+            data-cfasync="false"
+            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+            data-id="maglit"
+            data-description="Support me on Buy me a coffee!"
+            data-message="Hi, Welcome to MagLit!"
+            data-color="#FFDD00"
+            data-position="Right"
+            data-x_margin="18"
+            data-y_margin="18"
+          ></script>
+        </div>
+      )}
+    </>
   );
 };
 
